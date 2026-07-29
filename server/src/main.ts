5524de6f1ae4049f7de76afffa4fd358594e5f67
Badal Chand <badalchand636@gmail.com>
2026-07-29 11:01:46 +0545

    Initial project setup

---
+++ server/src/main.ts
@@ -0,0 +1,54 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Global Prefix
+  app.setGlobalPrefix('api');
+
+  // Validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  // Cookie Parser
+  app.use(cookieParser());
+
+  // CORS
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Hospital Management API')
+    .setDescription('Hospital Management System Backend')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT || 5000);
+
+  console.log(
+    `🚀 Server running at http://localhost:${process.env.PORT || 5000}/api`,
+  );
+
+  // console.log(
+  //   `📚 Swagger Docs: http://localhost:${process.env.PORT || 5000}/api/docs`,
+  // );
+}
+
+bootstrap();
---
+++ server/src/users/schemas/user.schema.ts
@@ -0,0 +1,76 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+import { Role } from "../enums/role.enum";
+
+
+
+export type UserDocument = HydratedDocument<User>;
+
+
+@Schema({
+    timestamps: true
+})
+
+
+export class User {
+    @Prop({
+        required: true,
+        trim: true
+    })
+    firstName!: string;
+
+
+    @Prop({
+        required: true,
+        trim: true
+    })
+    lastName!: string;
+
+    @Prop({
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    })
+    email!: string;
+
+    @Prop({
+        required: true
+    })
+    password!: string;
+
+    @Prop({
+        required: true
+    })
+    phone!: string;
+
+    @Prop({
+        type: String,
+        enum: Role,
+        default: Role.PATIENT
+    })
+    role!: Role;
+
+    @Prop({
+        default: false
+    })
+    isVerified!: boolean;
+
+    @Prop()
+    refreshToken?: string;
+
+    // Forgot Password OTP
+    @Prop()
+    passwordResetOtp?: string;
+
+    @Prop()
+    passwordResetOtpExpires?: Date;
+
+    @Prop({
+        default: "",
+    })
+    profileImage!: string;
+}
+
+
+export const UserSchema = SchemaFactory.createForClass(User)
