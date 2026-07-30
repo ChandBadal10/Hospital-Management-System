4039ee03502ad613f6999a43096fb01663fff47a
Badal Chand <badalchand636@gmail.com>
2026-07-30 10:39:34 +0545

    Completed the create department api

--- server/src/departments/departments.module.ts
+++ server/src/departments/departments.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { DepartmentsController } from './departments.controller';
 import { DepartmentsService } from './departments.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Department, DepartmentSchema } from './schemas/department.schema';
 
 @Module({
+
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Department.name,
+        schema: DepartmentSchema
+      }
+    ])
+  ],
   controllers: [DepartmentsController],
   providers: [DepartmentsService]
 })
