bd3a602a0f158642173759dffc4455e622838547
Badal Chand <badalchand636@gmail.com>
2026-07-30 18:02:25 +0545

    Completed the api of the get patietent by id

--- server/src/patients/patients.controller.ts
+++ server/src/patients/patients.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
 import { PatientsService } from './patients.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
@@ -7,6 +7,7 @@ import { Role } from 'src/users/enums/role.enum';
 import { CreatePatientDto } from './dto/create-patient.dto';
 import { GetUser } from 'src/auth/decorators/get-user.decorator';
 import type { CurrentUser } from 'src/auth/interfaces/current-user.interface';
+import { GetPatientsQueryDto } from './dto/get-patients-query.dto';
 
 @Controller('patients')
 export class PatientsController {
@@ -23,5 +24,27 @@ export class PatientsController {
         @GetUser() user: CurrentUser
     ) {
         return this.patientsService.createPatient(createPatientDto, user);
+    }
+
+    //Get All Patients
+
+    @Get()
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.ADMIN)
+    getAllPatients(
+        @Query() query: GetPatientsQueryDto,
+    ) {
+        return this.patientsService.getAllPatients(query);
+    }
+
+
+    //Get Patient by id
+    @Get(":id")
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.ADMIN)
+    getPatientById(
+        @Param("id") id: string,
+    ) {
+        return this.patientsService.getPatientById(id);
     }
 }
