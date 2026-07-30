67a54d261b7dd24f90ffda29286426620befeb59
Badal Chand <badalchand636@gmail.com>
2026-07-30 13:43:30 +0545

    Completed the Create Doctor by admin side and it saved in both as a doctor role and doctor info in database

---
+++ server/src/doctors/dto/create-doctor.dto.ts
@@ -0,0 +1,58 @@
+import {
+  IsArray,
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+} from "class-validator";
+
+export class CreateDoctorDto {
+  @IsString()
+  @IsNotEmpty()
+  firstName!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  lastName!: string;
+
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phone!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  departmentId!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  specialization!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  qualification!: string;
+
+  @IsNumber()
+  experience!: number;
+
+  @IsNumber()
+  consultationFee!: number;
+
+  @IsString()
+  @IsNotEmpty()
+  licenseNumber!: string;
+
+  @IsArray()
+  availableDays!: string[];
+
+  @IsString()
+  startTime!: string;
+
+  @IsString()
+  endTime!: string;
+
+  @IsString()
+  bio!: string;
+}
