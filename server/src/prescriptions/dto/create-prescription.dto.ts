a87af7b2fd7031eb5bf869aa34ae06ed6aac2a70
Badal Chand <badalchand636@gmail.com>
2026-08-04 10:33:09 +0545

    Working on the Prescription

---
+++ server/src/prescriptions/dto/create-prescription.dto.ts
@@ -0,0 +1,50 @@
+import {
+IsArray,
+IsMongoId,
+IsNotEmpty,
+IsOptional,
+IsString,
+ValidateNested,
+} from "class-validator";
+import { Type } from "class-transformer";
+
+class MedicineDto {
+
+@IsString()
+medicineName!: string;
+
+@IsString()
+dosage!: string;
+
+@IsString()
+frequency!: string;
+
+@IsString()
+duration!: string;
+
+@IsString()
+instruction!: string;
+
+}
+
+export class CreatePrescriptionDto {
+
+@IsMongoId()
+medicalRecord!: string;
+
+@IsMongoId()
+patient!: string;
+
+@IsMongoId()
+doctor!: string;
+
+@IsArray()
+@ValidateNested({ each: true })
+@Type(() => MedicineDto)
+medicines!: MedicineDto[];
+
+@IsOptional()
+@IsString()
+notes?: string;
+
+}
---
+++ server/src/prescriptions/prescriptions.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { PrescriptionsController } from './prescriptions.controller';
+import { PrescriptionsService } from './prescriptions.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Prescription, PrescriptionSchema } from './schemas/prescription.schema';
+import { MedicalRecord, MedicalRecordSchema } from 'src/medical-records/schemas/medical-record.schema';
+import { Patient, PatientSchema } from 'src/patients/schemas/patient.schema';
+import { Doctor, DoctorSchema } from 'src/doctors/schemas/doctor.schema';
+import { User, UserSchema } from 'src/users/schemas/user.schema';
+
+@Module({
+  imports:[
+    MongooseModule.forFeature([
+      {
+        name: Prescription.name,
+        schema: PrescriptionSchema
+      },
+      {
+        name: MedicalRecord.name,
+        schema: MedicalRecordSchema
+      },
+      {
+        name: Patient.name,
+        schema: PatientSchema
+      },
+      {
+        name: Doctor.name,
+        schema: DoctorSchema
+      },
+      {
+        name: User.name,
+        schema: UserSchema
+      }
+    ])
+  ],
+  controllers: [PrescriptionsController],
+  providers: [PrescriptionsService]
+})
+export class PrescriptionsModule {}
