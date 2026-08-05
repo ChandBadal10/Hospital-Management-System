4bb5bdb69fb6158d87e3f52dfed1b2cb9347ab47
Badal Chand <badalchand636@gmail.com>
2026-08-05 11:10:20 +0545

    Completed the create laboratory test apis

--- server/src/laboratory-tests/laboratory-tests.controller.ts
+++ server/src/laboratory-tests/laboratory-tests.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { LaboratoryTestsService } from './laboratory-tests.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/users/enums/role.enum';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import type { CurrentUser } from 'src/auth/interfaces/current-user.interface';
+import { CreateLaboratoryTestDto } from './dto/create-laboratory-test.dto';
 
 @Controller('laboratory-tests')
-export class LaboratoryTestsController {}
+export class LaboratoryTestsController {
+    constructor(
+        private readonly laboratoryTestsService: LaboratoryTestsService,
+    ) {}
+
+    @Post()
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.ADMIN)
+    createLaboratoryTest(
+        @Body() createLaboratoryTestDto: CreateLaboratoryTestDto,
+        @GetUser() user: CurrentUser
+    ) {
+        return this.laboratoryTestsService.createLaboratoryTest(createLaboratoryTestDto, user)
+    }
+}
--- server/src/laboratory-tests/laboratory-tests.service.ts
+++ server/src/laboratory-tests/laboratory-tests.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { LaboratoryTest, LaboratoryTestDocument } from './schemas/laboratory-test.schema';
+import { Model, Types } from 'mongoose';
+import { Laboratory, LaboratoryDocument } from 'src/laboratories/schemas/laboratory.schema';
+import { User, UserDocument } from 'src/users/schemas/user.schema';
+import { CreateLaboratoryTestDto } from './dto/create-laboratory-test.dto';
+import { CurrentUser } from 'src/auth/interfaces/current-user.interface';
 
 @Injectable()
-export class LaboratoryTestsService {}
+export class LaboratoryTestsService {
+    constructor(
+        @InjectModel(LaboratoryTest.name)
+        private laboratoryTestModel: Model<LaboratoryTestDocument>,
+
+        @InjectModel(Laboratory.name)
+        private laboratoryModel: Model<LaboratoryDocument>,
+
+        @InjectModel(User.name)
+        private userModel: Model<UserDocument>
+    ) {}
+
+
+    async createLaboratoryTest(createLaboratoryTestDto: CreateLaboratoryTestDto, user: CurrentUser) {
+
+        const laboratory = await this.laboratoryModel.findById(createLaboratoryTestDto.laboratory);
+        if(!laboratory) {
+            throw new NotFoundException("Laboratory not found")
+        }
+
+        const alreadyExists = await this.laboratoryTestModel.findOne({laboratory: createLaboratoryTestDto.laboratory, name: createLaboratoryTestDto.name, isActive: true});
+
+        if(alreadyExists) {
+            throw new BadRequestException("Laboratory test already exists")
+        }
+
+        const laboratoryTest = await this.laboratoryTestModel.create({
+            ...createLaboratoryTestDto,
+            createdBy: new Types.ObjectId(user.id),
+            updatedBy: new Types.ObjectId(user.id)
+        });
+
+
+        const data = await this.laboratoryTestModel.findById(laboratoryTest._id)
+        .populate("laboratory", "name location contactNumber")
+        .populate("createdBy", "firstName lastName")
+        .populate("updatedBy", "firstName lastName");
+
+        return {
+            success: true,
+            message: "Laboratory test created successfully",
+            data
+        }
+    }
+}
